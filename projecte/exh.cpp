cb18b09c779c99fe1694b5d669b3e761e5d5b605
Jose Pérez Cano <jose.perez.cano@est.fib.upc.edu>
2019-11-13 14:34:17 +0100

    Added two prunes(va como la seda)

--- projecte/exh.cpp
+++ projecte/exh.cpp
@@ -11,27 +11,46 @@ struct player{
   int price, points;
 };
 
+/* 
+  Functions for ordering players
+*/
 bool operator <(const player& a, const player& b){
   return a.points > b.points;
 }
 
+bool comp_price(const player& a, const player& b){
+  return a.price > b.price;
+}
+
+
+/* 
+  Struct for storing the player of the team
+*/
 struct football_team{
   vector<vector<player>> pos = vector<vector<player>>(4);
   int price = 0;
   int points = 0;
 };
 
+/* 
+  Global variables
+*/
 int n1, n2, n3, T, J;
+vector<int> lim;
 int max_points = 0;
 vector<vector<player>> db(4);
+vector<vector<player>> db_price(4);
 vector<vector<bool>> used(4);
 football_team team;
 string outputFile;
 
+// Returns actual time
 double now(){
   return clock()/double(CLOCKS_PER_SEC);
 }
 
+// Reads the database from argv[1] and stores it in db
+// Exception: Player with price 0 aren't taken into account
 void read(int argc, char** argv){
   if (argc != 4) {
     cout << "Syntax: " << argv[0] << " data_base.txt input.txt output.txt" << endl;
@@ -46,6 +65,7 @@ void read(int argc, char** argv){
     getline(in,name,';');  if (name == "") break;
     getline(in,pos,';');
     in >> price;
+    if (price == 0) break;
     char aux; in >> aux;
     getline(in,club,';');
     in >> points;
@@ -62,12 +82,12 @@ void read(int argc, char** argv){
   in = ifstream(argv[2]);
   in >> n1 >> n2 >> n3 >> T >> J;
   in.close();
+  lim = {1, n1, n2, n3};
 }
 
+// Outputs the team in the format asked into the outputFile
 void print(double t){
-  ofstream f(outputFile);
-  f.precision(1);
-  f.setf(ios::fixed);
+  ofstream f(outputFile); f.precision(1); f.setf(ios::fixed);
   f << t << endl;
   vector<string> zone = {"POR: ", "DEF: ", "MIG: ", "DAV: "};
   for (int j = 0; j < 4;j++){
@@ -80,28 +100,76 @@ void print(double t){
   f <<  "Punts: " << team.points << endl;
   f <<  "Preu: " << team.price << endl;
   f.close();
-  cout << "printed" << endl;
 }
 
+// Deletes players that costs more than J
+void clean(){
+  vector<vector<player>> copy = db;
+  db = vector<vector<player>>(4);
+  for (int j = 0; j < 4; j++){
+    for (int i = 0; i < copy[j].size(); i++){
+      if (copy[j][i].price <= J) db[j].push_back(copy[j][i]);
+    }
+  }
+}
+
+// Tells if the team is filled;
 bool filled(){
   return 11 == team.pos[0].size() + team.pos[1].size() + team.pos[2].size()
     + team.pos[3].size();
 }
 
-// p means position, 0: por, 1: def, 2: mig, 3: dav
+// Returns the maximum number of points that can be obtained
+// with the players remaining in each position
+int best_points(int idx, int p, int insd){
+  int points = 0;
+  for (int i = 0; i < lim[p]-insd; i++) points += db[p][idx+i].points;
+  for (int i = p+1; i < 4; i++){
+    for (int j = 0; j < lim[i]; j++) points += db[i][j].points;
+  }
+  return points;
+}
+
+// Returns the cheapest price of the players remaining in each position
+int best_price(int idx, int p, int insd){
+  int price = 0;
+  int n = db_price[p].size()-1;
+  for (int i = 0; i < lim[p]-insd; i++) price += db_price[p][n-i].price;
+  for (int i = p+1; i < 4; i++){
+    n = db_price[i].size()-1;
+    for (int j = 0; j < lim[i]; j++) price += db_price[i][n-j].price;
+  }
+  return price;
+}
+
+/* 
+  Looks for the best configuration of team with
+  allineation n1, n2, n3 and price no higher than T
+  idx: current player index
+  p: position, 0: por, 1: def, 2: mig, 3: dav
+  t1: time when the program started
+*/
 void find(int idx, int p, double& t1){
-  vector<int> lim = {1, n1, n2, n3};
+  // Base case
   if (filled() or (p == 3 and idx == db[p].size())){
     if(team.points >= max_points) {
       double t2 = now();
       print(t2 - t1);
       max_points = team.points;
     }
+  // The position p is already filled
   } else if (team.pos[p].size() == lim[p]) find(0, p+1, t1);
+  // There are players remaining 
   else if (not (p < 3 and idx == db[p].size())){
     player pl = db[p][idx];
-    if ((pl.price <= J and pl.price + team.price <= T) 
-    and (not used[p][idx])){
+    // There aren't enough players to fill position p
+    if (db[p].size() - idx < lim[p] - team.pos[p].size()) return;
+    // The points can't be improved
+    if (best_points(idx, p, team.pos[p].size()) + team.points <= max_points) return;
+    // We can't finish the team due to price
+    if (best_price(idx, p, team.pos[p].size()) + team.price > T) return;
+    // Player is cheap enough
+    if ((pl.price + team.price <= T) and (not used[p][idx])){
       used[p][idx] = true;
       team.pos[p].push_back(pl);
       team.price += pl.price; team.points += pl.points;
@@ -116,10 +184,13 @@ void find(int idx, int p, double& t1){
 }
 
 int main(int argc, char** argv) {
-  read(argc, argv);
-  //for (int i = 0; i < 4; i++) sort(db[i].begin(), db[i].end());
-  outputFile = argv[3];
-  for (int i = 0; i < 4; i++) used[i] = vector<bool>(db[i].size(), false);
   double t1 = now();
+  read(argc, argv);
+  clean();
+  for (int i = 0; i < 4; i++) used[i] = vector<bool>(db[i].size(), false);
+  for (int i = 0; i < 4; i++) sort(db[i].begin(), db[i].end());
+  db_price = db;
+  for (int i = 0; i < 4; i++) sort(db_price[i].begin(), db_price[i].end(), comp_price);
+  outputFile = argv[3];
   find(0, 0, t1);
 }
